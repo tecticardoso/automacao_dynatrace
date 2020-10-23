63158cb33b26a0640258cb448522aeb688419cb9
Tiago Cardoso <70172767+tecticardoso@users.noreply.github.com>
2020-10-23 11:37:53 -0300

    Add files via upload

---
+++ roles/restart_claro_satellite/files/restartWLS.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from java.lang import System
+from java.io import FileInputStream
+
+propInputStream = FileInputStream(sys.argv[1]+".properties")
+
+configProps = Properties()
+configProps.load(propInputStream)
+
+connect(configProps.get("user"),configProps.get("pass"),'t3://'+configProps.get("host")+':'+configProps.get("port"))
+
+shutdown(sys.argv[2],'Server',force='true')
+start(sys.argv[2],'Server')
+disconnect()
+exit()
+
+
